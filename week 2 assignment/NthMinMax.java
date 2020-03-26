d2e3c9c805f3326cbcb7fd93c1ab2df60187d56f
Rahul Rana <rahul_rana@epam.com>
2020-03-26 15:40:22 +0530

    week 2 assignment updated

--- week 2 assignment/NthMinMax.java
+++ week 2 assignment/NthMinMax.java
@@ -1,22 +1,47 @@
-package test;
-
-import java.util.Arrays;
+package com.program;
 import java.util.Scanner;
 
-//Program to find the nth minimum (or maximum) element of an array.
 public class NthMinMax {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int size = input.nextInt();
-		int ar[] = new int[size];
-		for (int counter = 0; counter < size; counter++) {
-			ar[counter] = input.nextInt();
-		}
-		int n = input.nextInt();
-		Arrays.parallelSort(ar);
-		System.out.println("nth smallest = " + ar[n - 1]);
-		System.out.println("nth largest = " + ar[size - n]);
-		input.close();
-	}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int ar[] = new int[size];
+        for (int i = 0; i < size; i++)
+            ar[i] = sc.nextInt();
+        int nthVal = sc.nextInt();
+        System.out.println("Nth minimum Number=" + nthVal(ar, 0, ar.length - 1, nthVal));
+    }
+
+    private static int partition(int[] ar, int l,
+                                 int r) {
+        int x = ar[r], i = l;
+        for (int j = l; j <= r - 1; j++) {
+            if (ar[j] <= x) {
+                int temp = ar[i];
+                ar[i] = ar[j];
+                ar[j] = temp;
+                i++;
+            }
+        }
+        int temp = ar[i];
+        ar[i] = ar[r];
+        ar[r] = temp;
+        return i;
+    }
+
+    private static int nthVal(int[] ar, int l,
+                              int r, int k) {
+        if (k > 0 && k <= r - l + 1) {
+            int pos = partition(ar, l, r);
+            if (pos - l == k - 1)
+                return ar[pos];
+            if (pos - l > k - 1)
+                return nthVal(ar, l, pos - 1, k);
+            else
+                return nthVal(ar, pos + 1, r, k - pos + l - 1);
+        }
+        return Integer.MAX_VALUE;
+    }
 
 }
